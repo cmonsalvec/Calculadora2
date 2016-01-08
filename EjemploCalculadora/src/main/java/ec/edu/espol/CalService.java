7aa44e17902f4b2caaa582fb059db0d0a97d81c8
Carlos Monsalve <monsalve@espol.edu.ec>
2016-01-08 09:51:25 -0500

    Primer Commit de Calculadora

---
+++ EjemploCalculadora/src/main/java/ec/edu/espol/CalService.java
@@ -0,0 +1,24 @@
+/* Este es un proyecto de prueba Gradle */
+
+package ec.edu.espol;
+
+public class CalService {
+
+	
+int b;
+	public ICalculator getCal() {
+		return cal;
+	}
+;
+
+{}
+	public void setCal(ICalculator cal) {
+		this.cal = cal;
+	}
+
+	ICalculator cal;
+	
+	public int addTwoNumbers(int x, int y){
+		return cal.add(x, y);
+	}
+}
